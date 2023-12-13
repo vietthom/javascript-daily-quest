5c9ffcffdc782a345375f37e846dbbef10f78dbc
vietthom <thomle0418@gmail.com>
2023-12-12 21:41:17 -0500

    repo init

---
+++ reverseWords.js
@@ -0,0 +1,17 @@
+// Function Name: "reverseWords"
+// Number of Parameters: 1
+// Description: The "reverseWords" function takes a string as input and reverses the order of the words in the string. The function should return the reversed string.
+
+// Technical Requirements:
+// The input parameter should be a string.
+// The function should handle inputs with multiple words separated by spaces.
+// The function should not use any built-in methods or libraries to reverse the words or the string.
+
+// Example:
+// reverseWords("Hello world") => "world Hello"
+
+// Unit Test:
+// The function should return an empty string when the input is an empty string.
+// The function should return the same word when the input is a single-word string.
+// The function should reverse the order of words in a string with multiple words.
+
